399ea0265d5194a420f377cc7f4d6302f5968687
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-04-14 15:15:29 -0600

    Atualzaicion de ajax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
   <script src="{{ asset('assets/js/core/bootstrap.min.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
   <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
-  <script type="text/javascript" src="{{ asset('assets/js/jquery-3.7.0.js') }}"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
   <script src="{{ asset('assets/js/plugins/datatables.js')}}"></script>
 
