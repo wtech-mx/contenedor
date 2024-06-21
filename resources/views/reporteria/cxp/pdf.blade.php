12ac430ce0bb45d6360f80029faabae00834da26
verplancken <42614785+verplancken@users.noreply.github.com>
2024-06-20 22:59:30 -0600

    cxp

--- resources/views/reporteria/cxp/pdf.blade.php
+++ resources/views/reporteria/cxp/pdf.blade.php
@@ -60,10 +60,6 @@
                         <th>Sobre peso</th>
                         <th>Estadia</th>
                         <th>Burreo</th>
-
-                        <th>Total oficial</th>
-                        <th>Total no oficial</th>
-                        <th>Importe CT</th>
                     </tr>
                 </thead>
                 <tbody style="text-align: center;font-size: 100%;">
@@ -90,28 +86,6 @@
                             <td>${{ number_format($cotizacion->Contenedor->Cotizacion->sobrepeso, 2, '.', ',') }}</td>
                             <td>${{ number_format($cotizacion->estadia, 2, '.', ',') }}</td>
                             <td>${{ number_format($cotizacion->burreo, 2, '.', ',') }}</td>
-
-                            <td>
-                                @php
-                                    $total_oficial = 0;
-                                    $total_oficial = ($cotizacion->Contenedor->Cotizacion->base_factura + $cotizacion->iva) - $cotizacion->retencion;
-                                @endphp
-                                $ {{ number_format($total_oficial, 2, '.', ',')}}
-                            </td>
-                            <td>
-                                @php
-                                    $total_no_ofi = 0;
-                                    $total_no_ofi = ($cotizacion->burreo + $cotizacion->estadia + $cotizacion->Contenedor->Cotizacion->sobrepeso + $cotizacion->otro + $cotizacion->precio) - $cotizacion->Contenedor->Cotizacion->base_factura;
-                                @endphp
-                                $ {{ number_format($total_no_ofi, 2, '.', ',')}}
-                            </td>
-                            <td>
-                                @php
-                                    $importe_vta = 0;
-                                    $importe_vta = $cotizacion->Contenedor->Cotizacion->base_taref + $total_oficial;
-                                @endphp
-                                $ {{ number_format($importe_vta, 2, '.', ',')}}
-                            </td>
                         </tr>
                     @endforeach
                 </tbody>
