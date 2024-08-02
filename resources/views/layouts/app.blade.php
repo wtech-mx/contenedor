16f0908739c144f192702ffa5e1936f23866f75d
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-08-02 07:37:49 -0600

    Contendir

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,9 @@
   <link href="{{ asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
   <link href="{{ asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
-  <script src="https://www.google.com/recaptcha/api.js?render=6LflbR0qAAAAADzEpS4m9oo_7Mftvt7K1OPHjC-D"></script>
   <link href="{{ asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />  <link href="{{ asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   @yield('css')
    <!-- Select2  -->
    <link rel="stylesheet" href="{{ asset('assets/vendor/select2/dist/css/select2.min.css')}}">
